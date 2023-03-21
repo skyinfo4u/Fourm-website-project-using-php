db0a9210b258cd9aef7e98a7c7bc6a2fcd3e0a61
Sandeep Kumar <97223348+skyinfo4u@users.noreply.github.com>
2023-03-22 00:18:55 +0530

    Add files via upload

---
+++ trail/forbackground.php
@@ -0,0 +1,14 @@
+
+    <style>
+        .back{
+            background-image: url(trail/2.jpeg);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 350px;
+        }
+    </style>
+
+    <div class="back">
+
+    </div>
